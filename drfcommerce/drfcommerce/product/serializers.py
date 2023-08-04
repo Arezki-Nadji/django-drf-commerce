04b5241e73a25755282571d6a366302b6d71b268
Arezki Mohamed Nadji <Arezki.m.nadji96@gmail.com>
2023-08-04 02:02:56 +0200

    corrected typo in Serializers.py added viewSet to Brand

--- drfcommerce/drfcommerce/product/serializers.py
+++ drfcommerce/drfcommerce/product/serializers.py
@@ -3,19 +3,19 @@ from rest_framework import serializers
 from .models import Brand, Category, Product
 
 
-class CategorySerialize(serializers.ModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = "__all__"
 
 
-class BrandCategory(serializers.ModelSerializer):
+class BrandSeriliazer(serializers.ModelSerializer):
     class Meta:
         model = Brand
         fields = "__all__"
 
 
-class ProductCategory(serializers.ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = "__all__"
--- drfcommerce/drfcommerce/product/views.py
+++ drfcommerce/drfcommerce/product/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
-from .models import Category
-from .serializers import CategorySerialize
+from .models import Category, Brand
+from .serializers import CategorySerializer, BrandSeriliazer
 from drf_spectacular.utils import extend_schema
 
 
@@ -12,7 +12,16 @@ class CategoryViewSet(viewsets.ViewSet):
 
     queryset = Category.objects.all()
 
-    @extend_schema(responses=CategorySerialize)
+    @extend_schema(responses=CategorySerializer)
     def list(self, request):
-        serializer = CategorySerialize(self.queryset, many=True)
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class BrandViewSet(viewsets.ViewSet):
+    queryset = Brand.objects.all()
+
+    @extend_schema(responses=BrandSeriliazer)
+    def list(self, request):
+        serializer = BrandSeriliazer(self.queryset, many=True)
         return Response(serializer.data)
