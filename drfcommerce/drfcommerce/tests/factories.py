0a6fbf363a926884d54dd1ff2726e9f3ff1c4d38
Arezki Mohamed Nadji <Arezki.m.nadji96@gmail.com>
2023-08-04 03:35:19 +0200

    added tests endpoint for Category, Brand and Product

--- drfcommerce/drfcommerce/tests/factories.py
+++ drfcommerce/drfcommerce/tests/factories.py
@@ -7,22 +7,22 @@ class CategoryFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Category
 
-    name = "test_category"
+    name = factory.Sequence(lambda n: "Category_%d" % n)
 
 
 class BrandFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Brand
 
-    name = "test_Brand"
+    name = factory.Sequence(lambda n: "Brand_%d" % n)
 
 
 class ProductFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Product
 
-    name = "test_product"
-    description = "test_description"
+    name = factory.Sequence(lambda n: "Category_%d" % n)
+    description = factory.Sequence(lambda n: "Description_%d" % n)
     is_digital = True
     brand = factory.SubFactory(BrandFactory)
     category = factory.SubFactory(CategoryFactory)
